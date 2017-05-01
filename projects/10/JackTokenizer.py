da031b5f660c0f44d4de906ac23a24103ef7e00e
bmcd87 <bradmcdermott@gmail.com>
2017-05-01 00:07:01 -0400

    Ch10 XML compiler working without expressions

--- projects/10/JackTokenizer.py
+++ projects/10/JackTokenizer.py
@@ -94,48 +94,196 @@ class CompileEngine:
     def __init__(self, tokenizer, filename):
         self.tokenizer = tokenizer
         self.output = open(filename, "w")
+        self.lines = list()
 
-    def compileClass():
+    def writeFile(self):
+        self.output.write("\n".join(self.lines))
+
+    def writeOpen(self, tag):
+        self.lines.append("<{}>".format(tag))
+
+    def writeClose(self, tag):
+        self.lines.append("</{}>".format(tag))
+
+    def writeTerminal(self, tag, value):
+        self.lines.append("<{}> {} </{}>".format(tag, value, tag))
+
+    def advance(self):
+        if(self.tokenizer.hasMoreTokens()):
+            self.tokenizer.advance()
+
+    def compileClass(self):
+        self.advance()
+        self.writeOpen("class")
+        self.compileItem() # class
+        self.compileItem() # name
+        self.compileItem() # {
+
+        while (self.tokenizer.keyWord in ["STATIC", "FIELD"]):
+            self.compileClassVarDec()
+
+        while (self.tokenizer.keyWord in ["CONSTRUCTOR", "FUNCTION", "METHOD"]):
+            self.compileSubroutine()
+
+        self.compileItem() # }
+        self.writeClose("class")
+
+    def compileClassVarDec(self):
+        self.writeOpen("classVarDec")
+        self.compileItem() # static/field
+        self.compileItem() # type
+        self.compileItem() # name
+        self.compileItem() # ;
+        self.writeClose("classVarDec")
+
+    def compileSubroutine(self):
+        self.writeOpen("subroutineDec")
+        self.compileItem() # constructor/function/method
+        self.compileItem() # return type
+        self.compileItem() # name
+        self.compileItem() # (
+        self.compileParameterList()
+        self.compileItem() # )
+        self.writeOpen("subroutineBody")
+        self.compileItem() # {
+        while(self.tokenizer.keyWord == "VAR"):
+            self.compileVarDec()
+
+        self.compileStatements()
+
+        self.compileItem() # }
+        self.writeClose("subroutineBody")
+        self.writeClose("subroutineDec")
+
+    def compileParameterList(self):
+        self.writeOpen("parameterList")
+        while(self.tokenizer.symbol != ")"):
+            self.compileItem() # type
+            self.compileItem() # name
+            if(self.tokenizer.symbol == ","):
+                self.compileItem() # ,
+
+        self.writeClose("parameterList")
+        
+
+    def compileVarDec(self):
+        self.writeOpen("varDec")
+        self.compileItem() # var
+        self.compileItem() # type
+        while(self.tokenizer.symbol != ";"):
+            self.compileItem() # name
+            if(self.tokenizer.symbol == ","):
+                self.compileItem() # ,
+        self.compileItem() # ;
+        self.writeClose("varDec")
+
+    def compileStatements(self):
+        self.writeOpen("statements")
+        while(self.tokenizer.symbol != "}"):
+            if(self.tokenizer.keyWord == "DO"):
+                self.compileDo()
+            elif(self.tokenizer.keyWord == "IF"):
+                self.compileIf()
+            elif(self.tokenizer.keyWord == "LET"):
+                self.compileLet()
+            elif(self.tokenizer.keyWord == "RETURN"):
+                self.compileReturn()
+            elif(self.tokenizer.keyWord == "WHILE"):
+                self.compileWhile()
+            else:
+                raise Exception()
+        self.writeClose("statements")
+
+    def compileDo(self):
+        self.writeOpen("doStatement")
+        self.compileItem() # do
+        while(self.tokenizer.symbol != "("):
+            self.compileItem() # Main . method
+        self.compileItem() # (
+        self.compileExpressionList()
+        self.compileItem() # )
+        self.compileItem() # ;
+        self.writeClose("doStatement")
+
+    def compileLet(self):
+        self.writeOpen("letStatement")
+        self.compileItem() # let
+        while(self.tokenizer.symbol != "="):
+            self.compileItem() # a 
+            if(self.tokenizer.symbol == "["):
+                self.compileItem() # [ 
+                self.compileExpression() # 0
+                self.compileItem() # ] 
+        self.compileItem() # =
+        self.compileExpression()
+        self.compileItem() # ;
+        self.writeClose("letStatement")
+
+    def compileWhile(self):
+        self.writeOpen("whileStatement")
+        self.compileItem() # while
+        self.compileItem() # (
+        self.compileExpression()
+        self.compileItem() # )
+        self.compileItem() # {
+        self.compileStatements()
+        self.compileItem() # }
+        self.writeClose("whileStatement")
+
+    def compileReturn(self):
+        self.writeOpen("returnStatement")
+        self.compileItem() # return
+        if(self.tokenizer.symbol != ";"):
+            self.compileExpression()
+        self.compileItem() # ;
+        self.writeClose("returnStatement")
+
+    def compileIf(self):
+        self.writeOpen("ifStatement")
+        self.compileItem() # if
+        self.compileItem() # (
+        self.compileExpression()
+        self.compileItem() # )
+        self.compileItem() # {
+        self.compileStatements()
+        self.compileItem() # }
+        if(self.tokenizer.keyWord == "ELSE"):
+            self.compileItem() # else
+            self.compileItem() # {
+            self.compileStatements()
+            self.compileItem() # }
+        self.writeClose("ifStatement")
+
+    def compileExpression(self):
+        self.writeOpen("expression")
+        while(self.tokenizer.symbol != ")" and self.tokenizer.symbol != "]" and self.tokenizer.symbol != ";"):
+            isIdent = self.tokenizer.tokenType == "IDENTIFIER"
+            if(isIdent):
+                self.writeOpen("term")
+            self.compileItem() # todo real expressions
+            if(isIdent):
+                self.writeClose("term")
+        self.writeClose("expression")
+
+    def compileItem(self):
+        if(self.tokenizer.tokenType == "KEYWORD"):
+            self.writeTerminal("keyword", self.tokenizer.keyWord.lower())
+        elif(self.tokenizer.tokenType == "IDENTIFIER"):
+            self.writeTerminal("identifier", self.tokenizer.identifier)
+        elif(self.tokenizer.tokenType == "SYMBOL"):
+            self.writeTerminal("symbol", self.tokenizer.symbol)
+        self.advance()
+
+    def compileTerm(self):
         pass
 
-    def compileClassVarDec():
-        pass
-
-    def compileSubroutine():
-        pass
-
-    def compileParameterList():
-        pass
-
-    def compileVarDec():
-        pass
-
-    def compileStatements():
-        pass
-
-    def compileDo():
-        pass
-
-    def compileLet():
-        pass
-
-    def compileWhile():
-        pass
-
-    def compileReturn():
-        pass
-
-    def compileIf():
-        pass
-
-    def compileExpression():
-        pass
-
-    def compileTerm():
-        pass
-
-    def compileExpressionList():
-        pass
+    def compileExpressionList(self):
+        self.writeOpen("expressionList")
+        while(self.tokenizer.symbol != ")"):
+            self.compileExpression()
+            if(self.tokenizer.symbol == ","):
+                self.compileItem() # ,
+        self.writeClose("expressionList")
 
 
 # JackAnalyzer
@@ -154,6 +302,7 @@ if (__name__ == "__main__"):
     for p in files:
         print("Handling " + p.name)
         tokenizer = JackTokenizer(p)
-        # compileEngine = CompileEngine(tokenizer, "{}/{}2.xml".format(dir, p.name))
-        tokenizer.writeTokenXml()
-        # compileEngine.compileClass()
+        compileEngine = CompileEngine(tokenizer, "{}/{}2.xml".format(dir, p.name.split(".")[0]))
+        #tokenizer.writeTokenXml()
+        compileEngine.compileClass()
+        compileEngine.writeFile()
